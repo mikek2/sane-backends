fc3093ade2159d3db3bc4574999e329d80496e0a
Michael Kotlyar <mdk369@nyu.edu>
2017-11-13 20:17:25 -0500

    Removed timestamps from footer to support reproducibility

--- tools/sane-desc.c
+++ tools/sane-desc.c
@@ -2816,8 +2816,8 @@ html_print_footer (void)
      "<address>\n"
      "<a href=\"http://www.sane-project.org/imprint.html\"\n"
      ">Contact</a>\n" "</address>\n" "<font size=-1>\n");
-  printf ("This page was last updated on %s by sane-desc %s from %s\n",
-	  asctime (localtime (&current_time)), SANE_DESC_VERSION, PACKAGE_STRING);
+  printf ("This page was last updated by sane-desc %s from %s\n",
+	  SANE_DESC_VERSION, PACKAGE_STRING);
   printf ("</font>\n");
   printf ("</body> </html>\n");
 }
